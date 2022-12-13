c2ffd97f87020a3afd8bd68fa9cece548c0e7d70
StepanGavrilov <gavrilovstepan78@gmail.com>
2022-12-13 23:51:10 +0300

    build(add contours)

--- badges.sh
+++ badges.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 flake8 src --exit-zero --format=html --statistics --tee --output-file reports/flake8/flake8stats.txt
 coverage run -m pytest --junitxml=reports/junit/junit.xml tests/test.py -vv -p no:cacheprovider
 coverage xml -o reports/coverage/coverage.xml
--- tests/test.py
+++ tests/test.py
@@ -1,1 +1,3 @@
+from __future__ import annotations
+
 from utest_users.test import UsersTest
